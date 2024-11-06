8317d0867b8f17f04fa251978c3235b9d51b89cb
sik9252 <sik9252@naver.com>
2024-11-06 23:08:14 +0900

    feat: OutputView에 환영인사 출력 메서드 구현

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -9,6 +9,11 @@ public class OutputView {
         this.productController = productController;
     }
 
+    public void printWelcome() {
+        System.out.println("안녕하세요. W편의점입니다.");
+        System.out.println("현재 보유하고 있는 상품입니다.\n");
+    }
+
     public void printProducts() {
         String productDisplay = productController.convertProductFormatToPrint();
         System.out.println(productDisplay);
