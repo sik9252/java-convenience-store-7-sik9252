7e1dca4fdf15ee7142ddfe01afd41b6bb87c02cf
sik9252 <sik9252@naver.com>
2024-11-06 22:33:20 +0900

    feat: product 클래스 생성

---
+++ src/main/java/store/model/Product.java
@@ -0,0 +1,31 @@
+package store.model;
+
+public class Product {
+    private final String name;
+    private final int price;
+    private final int quantity;
+    private final String promotion;
+
+    public Product(String name, int price, int quantity, String promotion) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotion = promotion;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getPromotion() {
+        return promotion;
+    }
+}
