00b6122803e05cb1c13195af694e8f2cb9261b83
sik9252 <sik9252@naver.com>
2024-11-06 23:07:52 +0900

    feat: OutputView 생성
    - printProducts: 출력 형식으로 가공된 상품 목록을 실제 화면에 출력한다.

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,8 +1,16 @@
 package store.view;
 
+import store.controller.ProductController;
+
 public class OutputView {
-    public void printWelcome() {
-        System.out.println("안녕하세요. W편의점입니다.");
-        System.out.println("현재 보유하고 있는 상품입니다.\n");
+    private final ProductController productController;
+
+    public OutputView(ProductController productController) {
+        this.productController = productController;
+    }
+
+    public void printProducts() {
+        String productDisplay = productController.convertProductFormatToPrint();
+        System.out.println(productDisplay);
     }
 }
