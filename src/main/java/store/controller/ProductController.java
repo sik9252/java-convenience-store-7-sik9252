a7891e591181e70ce7b4fab84c78ce4838634d9b
sik9252 <sik9252@naver.com>
2024-11-06 23:07:06 +0900

    feat: ProductController 생성
    - createProductsFromResource: 리소스를 기반으로 실제 상품을 생성한다.
    - convertProductFormatToPrint: 출력하기 위한 포맷으로 가공한다.
    - getQuantityInfo: 상품의 수량 정보를 가져와 반환한다.
    - getPromotionInfo: 상품의 프로모션 정보를 가져와 반환한다.

---
+++ src/main/java/store/controller/ProductController.java
@@ -0,0 +1,49 @@
+package store.controller;
+
+import store.model.Product;
+import store.service.ProductService;
+
+import java.util.List;
+
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    public void createProductsFromResource(String filename) {
+        productService.createProductsFromResource(filename);
+    }
+
+    public String convertProductFormatToPrint() {
+        List<Product> products = productService.getProducts();
+        StringBuilder result = new StringBuilder();
+
+        for (Product product : products) {
+            String quantityInfo = getQuantityInfo(product);
+            String promotionInfo = getPromotionInfo(product);
+
+            result.append(String.format("- %s %,d원 %s%s%n", product.getName(), product.getPrice(),
+                    quantityInfo, promotionInfo));
+        }
+
+        return result.toString();
+    }
+
+    private String getQuantityInfo(Product product) {
+        if (product.getQuantity() > 0) {
+            return product.getQuantity() + "개";
+        }
+
+        return "재고 없음";
+    }
+
+    private String getPromotionInfo(Product product) {
+        if (product.getPromotion() != null) {
+            return " " + product.getPromotion();
+        }
+
+        return "";
+    }
+}
