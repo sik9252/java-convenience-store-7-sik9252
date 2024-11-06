3fc109d804e48e884d9abe4b81dc7cef70d859ef
sik9252 <sik9252@naver.com>
2024-11-06 22:04:16 +0900

    feat: ProductRepository 구현
    - addProduct: products 리스트에 상품을 추가한다.
    - getAllProducts: 추가한 모든 상품을 가져온다.

---
+++ src/main/java/store/implement/ProductRepositoryImpl.java
@@ -0,0 +1,21 @@
+package store.implement;
+
+import store.model.Product;
+import store.repository.ProductRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductRepositoryImpl implements ProductRepository {
+    private final List<Product> products = new ArrayList<>();
+
+    @Override
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return products;
+    }
+}
