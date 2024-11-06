292bf5ecf1036456a9f9bc0b294a8a889e0c65ed
sik9252 <sik9252@naver.com>
2024-11-06 21:55:59 +0900

    feat: 환영인사 출력 기능 구현

---
+++ src/main/java/store/view/OutputView.java
@@ -0,0 +1,8 @@
+package store.view;
+
+public class OutputView {
+    public void printWelcome() {
+        System.out.println("안녕하세요. W편의점입니다.");
+        System.out.println("현재 보유하고 있는 상품입니다.\n");
+    }
+}
