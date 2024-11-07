21b74fd1b2d325c95637e76bae8243bf4f7f8360
sik9252 <sik9252@naver.com>
2024-11-07 13:19:30 +0900

    feat: 최종 컨트롤러인 StoreController에 promotions.md 파일을 불러와 저장하는 기능을 추가한다.

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -2,25 +2,31 @@ package store.controller;
 
 import store.factory.OrderFactory;
 import store.factory.ProductFactory;
+import store.factory.PromotionFactory;
+import store.service.ProductService;
+import store.service.PromotionService;
 import store.view.InputView;
 import store.view.OutputView;
 
 public class StoreController {
-    ProductController productController;
-    OrderController orderController;
-
     private final InputView inputView;
     private final OutputView outputView;
+    private final OrderController orderController;
+    private final ProductService productService;
+    private final PromotionService promotionService;
 
     public StoreController() {
-        productController = ProductFactory.createProductController();
+        productService = ProductFactory.createProductService();
+        promotionService = PromotionFactory.createPromotionService();
+        ProductController productController = new ProductController(productService);
         orderController = OrderFactory.createOrderController();
         outputView = new OutputView(productController);
         inputView = new InputView();
     }
 
     public void run() {
-        productController.createProductsFromResource("products.md");
+        productService.save("products.md");
+        promotionService.save("promotions.md");
         outputView.printWelcome();
         outputView.printProducts();
         orderController.order();
