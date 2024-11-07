40532e11297600cdb5c3837e5d7c9106f795b87a
sik9252 <sik9252@naver.com>
2024-11-07 13:18:36 +0900

    refactor: controller 대신 service 생성을 위임하도록 변경

--- src/main/java/store/factory/ProductFactory.java
+++ src/main/java/store/factory/ProductFactory.java
@@ -1,13 +1,12 @@
 package store.factory;
 
-import store.controller.ProductController;
 import store.implement.ProductRepositoryImpl;
 import store.service.ProductService;
 
 public class ProductFactory {
-    public static ProductController createProductController() {
+    public static ProductService createProductService() {
         ProductRepositoryImpl productRepository = new ProductRepositoryImpl();
-        ProductService productService = new ProductService(productRepository);
-        return new ProductController(productService);
+
+        return new ProductService(productRepository);
     }
 }
