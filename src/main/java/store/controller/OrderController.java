22da73442e8e43916c44ace070170c843e71b820
sik9252 <sik9252@naver.com>
2024-11-07 12:01:28 +0900

    feat: OrderController 생성
    - order: 사용자의 구매 상품에 대한 입력을 받고 주문을 생성한다.

---
+++ src/main/java/store/controller/OrderController.java
@@ -0,0 +1,20 @@
+package store.controller;
+
+import store.service.OrderService;
+import store.view.InputView;
+
+
+public class OrderController {
+    private final InputView inputView;
+    private final OrderService orderService;
+
+    public OrderController(InputView inputView, OrderService orderService) {
+        this.inputView = inputView;
+        this.orderService = orderService;
+    }
+
+    public void order() {
+        String input = inputView.getOrders();
+        orderService.createOrder(input);
+    }
+}
