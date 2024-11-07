6f3da225341e445643b18e0ddaf258b854130f4f
sik9252 <sik9252@naver.com>
2024-11-07 13:15:43 +0900

    feat: 프로모션 정보를 저장할 PromotionRepository 인터페이스 추가

---
+++ src/main/java/store/repository/PromotionRepository.java
@@ -0,0 +1,11 @@
+package store.repository;
+
+import store.model.Promotion;
+
+import java.util.List;
+
+public interface PromotionRepository {
+    void addPromotion(Promotion promotion);
+
+    List<Promotion> getPromotions();
+}
