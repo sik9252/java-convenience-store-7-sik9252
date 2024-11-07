fb454341b3af07fadca23601861858a6a385d7bd
sik9252 <sik9252@naver.com>
2024-11-07 13:15:20 +0900

    feat: Promotion 클래스 생성

---
+++ src/main/java/store/model/Promotion.java
@@ -0,0 +1,37 @@
+package store.model;
+
+public class Promotion {
+    private final String name;
+    private final int buy;
+    private final int get;
+    private final String startDate;
+    private final String endDate;
+
+    public Promotion(String name, int buy, int get, String startDate, String endDate) {
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBuy() {
+        return buy;
+    }
+
+    public int getGet() {
+        return get;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+}
