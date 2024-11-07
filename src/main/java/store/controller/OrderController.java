cbbf4012be75f93a4e61623b17ccc7fa46b519ce
sik9252 <sik9252@naver.com>
2024-11-08 01:01:31 +0900

    feat: 재입력 기능 추가, 입력 값 검증 메서드 추가
    - checkInputIsEmpty: 상품 구매 입력 값이 빈 경우
    - checkInputHasValidFormat: 올바른 상품 구매 입력 형식이 아닌 경우

--- src/main/java/store/controller/OrderController.java
+++ src/main/java/store/controller/OrderController.java
@@ -1,10 +1,16 @@
 package store.controller;
 
+import store.exception.CustomException;
 import store.service.OrderService;
 import store.view.InputView;
 
+import static store.exception.ErrorMessage.INVALID_INPUT;
+import static store.exception.ErrorMessage.INVALID_PURCHASE_FORMAT;
+
 
 public class OrderController {
+    private static final String PATTERN = "\\[([가-힣A-Za-z]+)-([1-9][0-9]*)](,\\[([가-힣A-Za-z]+)-([1-9][0-9]*)])*";
+
     private final InputView inputView;
     private final OrderService orderService;
 
@@ -14,7 +20,28 @@ public class OrderController {
     }
 
     public void order() {
-        String input = inputView.getOrders();
-        orderService.create(input);
+        while (true) {
+            try {
+                String input = inputView.getOrders();
+                checkInputIsEmpty(input);
+                checkInputHasValidFormat(input);
+                orderService.create(input);
+                break;
+            } catch (CustomException e) {
+                System.out.println(e.getMessage() + "\n");
+            }
+        }
+    }
+
+    private void checkInputIsEmpty(String input) {
+        if (input.isEmpty()) {
+            throw new CustomException(INVALID_INPUT.getMessage());
+        }
+    }
+
+    private void checkInputHasValidFormat(String input) {
+        if (!input.matches(PATTERN)) {
+            throw new CustomException(INVALID_PURCHASE_FORMAT.getMessage());
+        }
     }
 }
