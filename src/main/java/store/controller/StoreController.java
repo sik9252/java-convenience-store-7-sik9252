998e69152dd4da1c9ad9088f53ea0b62041cb0e2
sik9252 <sik9252@naver.com>
2024-11-07 12:01:53 +0900

    feat: 최종 컨트롤러인 StoreController에 order 기능을 추가한다.

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,20 +1,28 @@
 package store.controller;
 
+import store.factory.OrderFactory;
 import store.factory.ProductFactory;
+import store.view.InputView;
 import store.view.OutputView;
 
 public class StoreController {
-    ProductController productController = ProductFactory.createProductController();
+    ProductController productController;
+    OrderController orderController;
 
+    private final InputView inputView;
     private final OutputView outputView;
 
     public StoreController() {
+        productController = ProductFactory.createProductController();
+        orderController = OrderFactory.createOrderController();
         outputView = new OutputView(productController);
+        inputView = new InputView();
     }
 
     public void run() {
         productController.createProductsFromResource("products.md");
         outputView.printWelcome();
         outputView.printProducts();
+        orderController.order();
     }
 }
