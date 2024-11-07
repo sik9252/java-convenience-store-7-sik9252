db013e0c40dd1eaee0a41ad79a6331f263b7ec3b
sik9252 <sik9252@naver.com>
2024-11-08 01:03:14 +0900

    feat: 구매할 상품 입력 후 검증 메서드 추가
    - checkProductExist: 해당 상품이 상품 목록에 존재하는지
    - checkQuantityAvailableToBuy: 구매하기에 충분한 수량이 존재하는지

--- src/main/java/store/service/OrderService.java
+++ src/main/java/store/service/OrderService.java
@@ -1,15 +1,27 @@
 package store.service;
 
+import store.exception.CustomException;
 import store.implement.OrderRepositoryImpl;
+import store.implement.ProductRepositoryImpl;
+import store.implement.PromotionRepositoryImpl;
 import store.model.Order;
 import store.utils.StringUtils;
 
 import java.util.List;
 
+import static store.exception.ErrorMessage.EXCEEDED_STOCK_QUANTITY;
+import static store.exception.ErrorMessage.PRODUCT_NOT_EXIST;
+
 public class OrderService {
+    private final ProductRepositoryImpl productRepository;
+    private final PromotionRepositoryImpl promotionRepository;
     private final OrderRepositoryImpl orderRepository;
 
-    public OrderService(OrderRepositoryImpl orderRepository) {
+    public OrderService(ProductRepositoryImpl productRepository,
+                        PromotionRepositoryImpl promotionRepository,
+                        OrderRepositoryImpl orderRepository) {
+        this.productRepository = productRepository;
+        this.promotionRepository = promotionRepository;
         this.orderRepository = orderRepository;
     }
 
@@ -26,8 +38,23 @@ public class OrderService {
             String name = processedInfo[0];
             int quantity = Integer.parseInt(processedInfo[1]);
 
+            checkProductExist(name);
+            checkQuantityAvailableToBuy(name, quantity);
+
             Order order = new Order(name, quantity);
             saveToRepository(order);
+        }
+    }
+
+    private void checkProductExist(String name) {
+        if (!productRepository.isProductExist(name)) {
+            throw new CustomException(PRODUCT_NOT_EXIST.getMessage());
+        }
+    }
+
+    private void checkQuantityAvailableToBuy(String name, int quantityToBuy) {
+        if (!productRepository.isQuantityAvailable(name, quantityToBuy)) {
+            throw new CustomException(EXCEEDED_STOCK_QUANTITY.getMessage());
         }
     }
 
@@ -36,5 +63,4 @@ public class OrderService {
             orderRepository.addOrder(order);
         }
     }
-
 }
