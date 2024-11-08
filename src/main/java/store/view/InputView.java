8027e2bbf2d73079b8d90561cfc9fa916433b6ba
sik9252 <sik9252@naver.com>
2024-11-08 18:27:00 +0900

    refactor: 띄어쓰기 수정

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -4,12 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public String getOrders() {
-        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+        System.out.println("\n" + "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
         return Console.readLine();
     }
 
-    public String getAnswerToPromotionInfo(String productName) {
-        System.out.println("현재" + productName + "은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
+    public String getAnswerToPromotionInfo(String productName, int freeQuantity) {
+        System.out.println("\n" + "현재 " + productName + "은(는) " + freeQuantity + "개를 무료로 더 받을 수 있습니다. " +
+                "추가하시겠습니까? (Y/N)");
         return Console.readLine();
     }
 }
