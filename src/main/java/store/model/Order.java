ad1c4b40f3dd1702a464e0577aacd271d485fa32
sik9252 <sik9252@naver.com>
2024-11-09 01:48:45 +0900

    refactor: 하나의 생성자로 통일

--- src/main/java/store/model/Order.java
+++ src/main/java/store/model/Order.java
@@ -2,39 +2,29 @@ package store.model;
 
 public class Order {
     private final String name;
-    private int buyQuantity;
-    private int freeQuantity;
+    private final int quantity;
     private int totalPrice;
 
-    public Order(String name, int price, int buyQuantity) {
+    public Order(String name, int price, int quantity) {
         this.name = name;
-        this.buyQuantity = buyQuantity;
+        this.quantity = quantity;
         this.totalPrice = 0;
-        calculateTotalPrice(price, buyQuantity);
-    }
-
-    public Order(String name, int freeQuantity) {
-        this.name = name;
-        this.freeQuantity = freeQuantity;
+        getTotalPrice(price, quantity);
     }
 
     public String getName() {
         return name;
     }
 
-    public int getBuyQuantity() {
-        return buyQuantity;
-    }
-
-    public int getFreeQuantity() {
-        return freeQuantity;
+    public int getQuantity() {
+        return quantity;
     }
 
     public int getTotalPrice() {
         return totalPrice;
     }
 
-    private void calculateTotalPrice(int price, int buyQuantity) {
+    private void getTotalPrice(int price, int buyQuantity) {
         totalPrice = buyQuantity * price;
     }
 }
