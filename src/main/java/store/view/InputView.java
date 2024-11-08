639331097fac400ee2fd0f5f78d421e11f7a3e22
sik9252 <sik9252@naver.com>
2024-11-08 13:08:40 +0900

    feat: 혜택에 대한 안내 메시지 및 입력 메서드 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -7,4 +7,9 @@ public class InputView {
         System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
         return Console.readLine();
     }
+
+    public String getAnswerToPromotionInfo(String productName) {
+        System.out.println("현재" + productName + "은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
+        return Console.readLine();
+    }
 }
