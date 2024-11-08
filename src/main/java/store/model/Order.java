0c7f1e200db812c5a282765c5a5d80d01fafa609
sik9252 <sik9252@naver.com>
2024-11-08 18:29:33 +0900

    feat: 총 구매와 프로모션 증정으로 인한 구매를 나누어 생성할 수 있도록 변경하고 합계를 계산하는 메서드 추가

--- src/main/java/store/model/Order.java
+++ src/main/java/store/model/Order.java
@@ -2,24 +2,39 @@ package store.model;
 
 public class Order {
     private final String name;
-    private final int quantity;
-    private final int totalPrice;
+    private int buyQuantity;
+    private int freeQuantity;
+    private int totalPrice;
 
-    public Order(String name, int quantity, int totalPrice) {
+    public Order(String name, int price, int buyQuantity) {
         this.name = name;
-        this.quantity = quantity;
-        this.totalPrice = totalPrice;
+        this.buyQuantity = buyQuantity;
+        this.totalPrice = 0;
+        calculateTotalPrice(price, buyQuantity);
+    }
+
+    public Order(String name, int freeQuantity) {
+        this.name = name;
+        this.freeQuantity = freeQuantity;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getQuantity() {
-        return quantity;
+    public int getBuyQuantity() {
+        return buyQuantity;
+    }
+
+    public int getFreeQuantity() {
+        return freeQuantity;
     }
 
     public int getTotalPrice() {
         return totalPrice;
+    }
+
+    private void calculateTotalPrice(int price, int buyQuantity) {
+        totalPrice = buyQuantity * price;
     }
 }
