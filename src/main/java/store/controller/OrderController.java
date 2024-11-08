72ddbc1f12d673cfa962dc8b5bd747330c9058ef
sik9252 <sik9252@naver.com>
2024-11-08 20:01:53 +0900

    feat: 구매 입력 정보를 바탕으로 주문 생성하는 기능 구현
    - 구매 정보를 입력받는다.
    - 구매 정보 입력을 가공한다.
    - 구매하려는 상품을 검증한다(존재하는지, 재고가 있는지)
    - 구매 상품의 프로모션을 확인한다
    - 프로모션으로 받을 수 있는 수량까지 정확하게 가져왔는지 확인한다.
    - 최종 주문을 생성한다.

--- src/main/java/store/controller/OrderController.java
+++ src/main/java/store/controller/OrderController.java
@@ -1,7 +1,11 @@
 package store.controller;
 
 import store.exception.CustomException;
+import store.model.Order;
 import store.service.OrderService;
+import store.service.ProductService;
+import store.service.PromotionService;
+import store.utils.StringUtils;
 import store.view.InputView;
 
 import static store.exception.ErrorMessage.INVALID_INPUT;
@@ -13,10 +17,18 @@ public class OrderController {
 
     private final InputView inputView;
     private final OrderService orderService;
+    private final ProductService productService;
+    private final PromotionService promotionService;
 
-    public OrderController(InputView inputView, OrderService orderService) {
+    public OrderController(
+            InputView inputView,
+            OrderService orderService,
+            ProductService productService,
+            PromotionService promotionService) {
         this.inputView = inputView;
         this.orderService = orderService;
+        this.productService = productService;
+        this.promotionService = promotionService;
     }
 
     public void order() {
@@ -25,12 +37,72 @@ public class OrderController {
                 String input = inputView.getOrders();
                 checkInputIsEmpty(input);
                 checkInputHasValidFormat(input);
-                orderService.create(input);
+                createEntireOrder(input);
                 break;
             } catch (CustomException e) {
-                System.out.println(e.getMessage() + "\n");
+                System.out.println(e.getMessage());
             }
         }
+    }
+
+    private void createEntireOrder(String input) {
+        String[] orderInfo = StringUtils.splitStringWithComma(input);
+
+        for (String info : orderInfo) {
+            String[] processedInfo = info.replace("[", "").replace("]", "").
+                    split("-");
+            String productName = processedInfo[0];
+            int price = productService.getProductPrice(productName);
+            int buyQuantity = Integer.parseInt(processedInfo[1]);
+
+            productService.checkProductExist(productName);
+            productService.checkQuantityAvailableToBuy(productName, buyQuantity);
+            int freeQuantity = getFreeQuantity(productName, buyQuantity);
+
+            createBuyOrder(productName, price, buyQuantity + freeQuantity);
+        }
+    }
+
+    private int getFreeQuantity(String productName, int buyQuantity) {
+        String promotionName = productService.getProductPromotion(productName);
+
+        if (promotionName != null) {
+            int[] benefit = promotionService.getBenefitOfPromotion(promotionName);
+            int buy = benefit[0];
+            int get = benefit[1];
+
+            return compareQuantityToGetFree(productName, buyQuantity, buy, get);
+        }
+
+        return 0;
+    }
+
+    public int compareQuantityToGetFree(String productName, int buyQuantity, int buy, int get) {
+        int promotionSet = buyQuantity / buy;
+        int remainder = Math.abs((buy + get) * promotionSet - buyQuantity);
+
+        if (remainder > 0) {
+            while (true) {
+                try {
+                    String input = inputView.getAnswerToPromotionInfo(productName, remainder);
+                    checkIsValidAnswerToPromotionInfo(input);
+
+                    if (input.equals("Y")) {
+                        createPromotionOrder(productName, remainder);
+                    }
+
+                    if (input.equals("N")) {
+                        return 0;
+                    }
+
+                    break;
+                } catch (CustomException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+
+        return remainder;
     }
 
     private void checkInputIsEmpty(String input) {
@@ -42,6 +114,24 @@ public class OrderController {
     private void checkInputHasValidFormat(String input) {
         if (!input.matches(PATTERN)) {
             throw new CustomException(INVALID_PURCHASE_FORMAT.getMessage());
+        }
+    }
+
+    private void checkIsValidAnswerToPromotionInfo(String input) {
+        if (!input.equals("Y") && !input.equals("N")) {
+            throw new CustomException(INVALID_INPUT.getMessage());
+        }
+    }
+
+    public void createBuyOrder(String name, int price, int buyQuantity) {
+        Order buyOrder = new Order(name, price, buyQuantity);
+        orderService.saveBuyOrderToRepository(buyOrder);
+    }
+
+    public void createPromotionOrder(String name, int freeQuantity) {
+        if (freeQuantity > 0) {
+            Order promotionOrder = new Order(name, freeQuantity);
+            orderService.savePromotionOrderToRepository(promotionOrder);
         }
     }
 }
