32f5c3419259aafafb0a838f12bf3daa380066d7
sik9252 <sik9252@naver.com>
2024-11-09 12:21:25 +0900

    feat: 멤버십 할인 여부를 묻는 입력 메서드 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public String getOrders() {
-        System.out.println("\n" + "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
         return Console.readLine();
     }
 
@@ -17,6 +17,11 @@ public class InputView {
     public String getAnswerToPromotionIsOutOfStock(String productName, int quantity) {
         System.out.println("\n" + "현재 " + productName + " " + quantity + "개는 " + "프로모션 할인이 적용되지 않습니다. " +
                 "그래도 구매하시겠습니까? (Y/N)");
+        return Console.readLine();
+    }
+
+    public String getAnswerToMemberShipDiscount() {
+        System.out.println("\n" + "멤버십 할인을 받으시겠습니까? (Y/N)");
         return Console.readLine();
     }
 }
